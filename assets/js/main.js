b4e7396a01fe993b510b1841fa76d4a1b1330478
Juan Da Silva <jcda_23@hotmail.com>
2022-05-18 12:31:53 -0500

    Se amplifo la informacion del package.jason se modificó la estructura de la pagina de guia paramejor posicionamiento de las imagenes y el response, se mejoro el apartado de SEO, se corrigieron algunos colores y tamaños de titulos en el response

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,30 @@
-// Carousel de la pagina principal seccion productos
-import {} from "../../src/js/swiper-bundle.min.js";
+/******** SCRIPT DE APERTURA Y CIERRE DE MENU MOBILE******* */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".navbar");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
+
+((d) => {
+  let video = d.querySelector(".video-guid");
+  window.addEventListener("scroll", function () {
+    let value = 1 + window.scrollY / -600;
+    video.style.opacity = value;
+  });
+})(document);
 
 var swiper = new Swiper(".swiper-container", {
   navigation: {
@@ -50,31 +75,3 @@ var swiper = new Swiper(".swiper-container", {
     },
   },
 });
-
-/******** SCRIPT DE APERTURA Y CIERRE DE MENU MOBILE******* */
-((d) => {
-  const $btnMenu = d.querySelector(".menu-btn"),
-    $menu = d.querySelector(".navbar");
-
-  $btnMenu.addEventListener("click", (e) => {
-    $btnMenu.firstElementChild.classList.toggle("none");
-    $btnMenu.lastElementChild.classList.toggle("none");
-    $menu.classList.toggle("is-active");
-  });
-
-  d.addEventListener("click", (e) => {
-    if (!e.target.matches(".menu a")) return false;
-
-    $btnMenu.firstElementChild.classList.remove("none");
-    $btnMenu.lastElementChild.classList.add("none");
-    $menu.classList.remove("is-active");
-  });
-})(document);
-
-((d) => {
-  let video = d.querySelector(".video-guid");
-  window.addEventListener("scroll", function () {
-    let value = 1 + window.scrollY / -600;
-    video.style.opacity = value;
-  });
-})(document);
