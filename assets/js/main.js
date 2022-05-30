4d009b12bce1426f61e3867c399589815269e764
Juan Da Silva <jcda_23@hotmail.com>
2022-05-29 22:04:18 -0500

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
+(function () {
+  $(document).ready(function () {
+    $(".btn-menu").click(function (e) {
+      e.preventDefault();
+      var filtro = $(this).attr("data-filter");
+
+      if (filtro == "todos") {
+        $(".box-img").show(500);
+      } else {
+        $(".box-img")
+          .not("." + filtro)
+          .hide(500);
+        $(".box-img")
+          .filter("." + filtro)
+          .show(500);
+      }
+    });
+
+    $(".categories a").click(function () {
+      $(this).addClass("activo").siblings().removeClass("activo");
+    });
+  });
+})();
+
 /******** SCRIPT DE APERTURA Y CIERRE DE MENU MOBILE******* */
 ((d) => {
   const $btnMenu = d.querySelector(".menu-btn"),
